650d5dbb0be2692e8bb4d63be20d1b6c27aa355d
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2025-03-24 10:26:37 -0400

    Create Regex.pl

---
+++ Regex.pl
@@ -0,0 +1,15 @@
+
+regex(Lista) :-
+    musthaveAB(Lista),
+    mustNotAA(Lista),
+    mustNotCCB(Lista).
+
+musthaveAB(Lista) :- isIn(['A','B'], Lista).
+
+mustNotAA(Lista) :- \+ isIn(['A','A'], Lista).
+
+mustNotCCB(Lista) :- \+ isIn(['C','C','B'], Lista).
+
+isIn(must, List) :-
+    append(_, RList, List),
+    append(must, _, RList).
